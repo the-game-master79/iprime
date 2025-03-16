0823a1081f08a6e60c33ccb3d4f07a92109b2157
oddkings <oddkingz12@gmail.com>
2025-03-16 20:22:26 +0530

    all files deleted

--- supabase/test/test_profile_policies.sql
+++
@@ -1,8 +0,0 @@
--- Test as regular user
-SELECT * FROM profiles WHERE user_id = auth.uid();
-
--- Test as admin
-SELECT * FROM profiles;
-
--- Test referral access
-SELECT * FROM profiles WHERE referred_by = (SELECT referral_code FROM current_user_profile);
