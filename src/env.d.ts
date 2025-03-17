cc0cf4e0987f26be8c786cf3d0ac8036cc0b4a0c
oddkings <oddkingz12@gmail.com>
2025-03-17 18:04:05 +0530

    supabase update

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,6 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
-  readonly VITE_HCAPTCHA_SITE_KEY: string;
-  readonly VITE_TAWKTO_PROPERTY_ID: string;
   // Add any other env variables you need
 }
 
